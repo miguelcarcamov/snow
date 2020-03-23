3f97eeae29ebe4abe6ee9eb838b542dac4292d8c
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2020-03-23 21:00:11 +0000

    fix

--- selfcal.py
+++ selfcal.py
@@ -3,19 +3,25 @@ import numpy as np
 
 class Selfcal:
 
-    def __init__(self, visfile="", imagename="", minblperant=0, refant="", spwmap=[], Imager=None):
+    def __init__(self, visfile="", imagename="", minblperant=0, refant="", spwmap=[], Imager=None, want_plot=False):
         self.visfile = visfile
         self.imagename = imagename
         self.minblperant = minblperant
         self.refant = refant
         self.spwmap = spwmap
         self.Imager = Imager
+        self.want_plot = want_plot
+
+    def plot_selfcal(caltable, want_plot=False):
+        if want_plot:
+            plotcal(caltable=caltable, xaxis='time', yaxis='amp', timerange='',
+                    iteration='antenna', subplot=421, plotrange=[0.0, 0.2, 1.8])
 
 
 class Ampcal(Selfcal):
     def __init__(self, minsnr=1.0, solint=[], combine="", phs_caltable="", selfcal_object=None):
         super().__init__(selfcal_object.visfile, selfcal_object.imagename, selfcal_object.minblperant,
-                         selfcal_object.refant, selfcal_object.spwmap, selfcal_object.Imager)
+                         selfcal_object.refant, selfcal_object.spwmap, selfcal_object.Imager, selfcal_object.want_plot)
         self.calmode = 'ap'
         self.minsnr = minsnr
         self.solint = solint
@@ -30,7 +36,7 @@ class Ampcal(Selfcal):
             gaincal(vis=self.visfile, field=Imager.getField(), caltable=caltable, spw=Imager.getspw(), gaintype='G', refant=self.refant, calmode=self.calmode,
                     combine=self.combine, solint=self.solint[i], minsnr=self.minsnr, minblperant=minblperant, gaintable=self.phs_caltable, spwmap=self.spwmap, solnorm=True)
 
-            plot_selfcal(caltable)
+            plot_selfcal(caltable, True)
 
             applycal(vis=self.visfile, spwmap=[self.spwmap, self.spwmap], field=Imager.getField(), gaintable=[
                      self.phs_caltable, caltable], gainfield='', calwt=False, flagbackup=False, interp='linearperobs')
@@ -66,7 +72,7 @@ class Phasecal(Selfcal):
             gaincal(vis=self.visfile, caltable=caltable, field=Imager.getField(), spw=Imager.getspw(), gaintype='G', refant=self.refant,
                     calmode=self.calmode, combine=self.combine, solint=self.solint[i], minsnr=self.minsnr, minblperant=self.minblperant)
 
-            plot_selfcal(caltable)
+            plot_selfcal(caltable, True)
 
             applycal(vis=self.visfile, field=Imager.getField(), spwmap=self.spwmap, gaintable=[
                      caltable], gainfield='', calwt=False, flagbackup=False, interp='linearperobs')
