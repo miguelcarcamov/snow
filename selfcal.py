d35289b642ae4b4b9fed0b7d312f5bc7a8ee8c34
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2020-03-24 18:25:19 +0000

    fix

--- selfcal.py
+++ selfcal.py
@@ -19,7 +19,7 @@ class Selfcal(object):
 
 
 class Ampcal(Selfcal):
-    def __init__(self, minsnr=1.0, solint=[], combine="", phs_caltable="", selfcal_object):
+    def __init__(self, selfcal_object, minsnr=1.0, solint=[], combine="", phs_caltable=""):
         super(Ampcal, self).__init__(selfcal_object.visfile, selfcal_object.imagename, selfcal_object.minblperant,
                          selfcal_object.refant, selfcal_object.spwmap, selfcal_object.Imager, selfcal_object.want_plot)
         self.calmode = 'ap'
@@ -50,7 +50,7 @@ class Ampcal(Selfcal):
 
 
 class Phasecal(Selfcal):
-    def __init__(self, minsnr=1.0, solint=[], combine="", selfcal_object):
+    def __init__(self, selfcal_object, insnr=1.0, solint=[], combine=""):
         super(Phasecal, self).__init__(selfcal_object.visfile, selfcal_object.imagename, selfcal_object.minblperant,
                          selfcal_object.refant, selfcal_object.spwmap, selfcal_object.Imager, selfcal_object.want_plot)
         self.calmode = 'p'
