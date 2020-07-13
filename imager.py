305a637967c27cf4f736e860bdcd447e4f22c851
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2020-07-12 21:54:39 -0400

    updating imager

--- imager.py
+++ imager.py
@@ -19,7 +19,7 @@ class Imager(object):
 
 class Clean(Imager):
     def __init__(self, specmode="", deconvolver="", nterms=1, threshold=0.0, interactive=False, gridder="", pbcor=False,
-                 savemodel="", usepointing=False, usemask="", negativethreshold=0.0, lownoisethreshold=1.5 , noisethreshold=4.25 ,
+                 savemodel="", usemask="", negativethreshold=0.0, lownoisethreshold=1.5 , noisethreshold=4.25 ,
                  sidelobethreshold=2.0, minbeamfrac=0.3 , imager_object=None):
         super(Clean, self).__init__(imager_object.inputvis, imager_object.output, imager_object.niter, imager_object.M, imager_object.N,
                                     imager_object.deltax, imager_object.stokes, imager_object.datacolumn, imager_object.robust, imager_object.field)
@@ -33,8 +33,8 @@ class Clean(Imager):
         tclean(vis=self.inputvis, imagename=imagename, field=self.field,
                datacolumn=self.datacolumn, specmode=self.specmode, stokes=self.stokes, deconvolver=self.deconvolver, nterms=self.nterms,
                imsize=imsize, cell=self.deltax, weighting="briggs", robust=self.robust, niter=self.niter, threshold=self.threshold,
-               interactive=self.interactive, gridder=self.gridder, pbcor=self.pbcor, savemodel=self.savemodel, usepointing=self.usepointing,
-               usemask=self.usemask, negativethreshold=self.negativethreshold, lownoisethreshold=self.lownoisethreshold, noisethreshold=self.noisethreshold,
+               interactive=self.interactive, gridder=self.gridder, pbcor=self.pbcor, savemodel=self.savemodel, usemask=self.usemask,
+               negativethreshold=self.negativethreshold, lownoisethreshold=self.lownoisethreshold, noisethreshold=self.noisethreshold,
                sidelobethreshold=self.sidelobethreshold, minbeamfrac=self.minbeamfrac)
 
 
