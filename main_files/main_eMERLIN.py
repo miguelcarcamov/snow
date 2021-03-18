9915c512b8e6d61d40b3341f1b16ee49e03762e7
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2021-03-18 18:41:21 -0300

    FIX: eMERLIN main script

--- main_files/main_eMERLIN.py
+++ main_files/main_eMERLIN.py
@@ -1,14 +1,20 @@
 from selfcalframework.selfcal import *
 from selfcalframework.imager import *
+from casatasks import flagdata
+from casatasks import mstransform
 import sys
 import numpy as np
 
 if __name__ == '__main__':
+
     visfile = sys.argv[3]
     output = sys.argv[4]
     want_plot = eval(sys.argv[5])
 
-    clean_imager = Clean(inputvis=visfile, output=output, niter=10000, M=1024, N=1024, cell="0.02arcsec",
+    selfcal_vis = visfile[:-3]+".flagged.ms"
+    mstransform(vis=visfile, outputvis=selfcal_vis, datacolumn="corrected")
+
+    clean_imager = Clean(inputvis=selfcal_vis, output=output, niter=10000, M=1024, N=1024, cell="0.02arcsec",
                          stokes="I", datacolumn="corrected", robust=2.0, scales=[0, 3, 5, 10 ,15, 20, 30, 40, 50, 80], specmode="mfs", deconvolver="multiscale", gridder="standard",
                          pbcor=True, savemodel=True, nsigma=4.0, interactive=False, cycleniter=100, usemask='auto-multithresh', sidelobethreshold=1.0, noisethreshold=8.0,
                          minbeamfrac=0.2, lownoisethreshold=1.5, negativethreshold=0.0)
@@ -24,6 +30,24 @@ if __name__ == '__main__':
     phscal = Phasecal(minsnr=2.0, solint=solint_phs,
                       combine="spw", varchange=varchange_phs, Imager=clean_imager, **shared_vars_dict)
 
+    # Backup MS to the state before self-cal
+    flagmanager(vis=selfcal_vis, mode='save', versionname='before_selfcal_flagging')
+
+    # Flag residual RFI
+    mode="rflag"
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="0", correlation="LL,RR", mode=mode, action="apply", flagbackup=False)
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="0", mode="extend", extendflags=False, action="apply", flagbackup=False)
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="1~2", correlation="LL,RR", mode=mode, action="apply", flagbackup=False)
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="1~2", mode="extend", extendflags=False, action="apply", flagbackup=False)
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="3~5", correlation="LL,RR", mode=mode, action="apply", flagbackup=False)
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="3~5", mode="extend", extendflags=False, action="apply", flagbackup=False)
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="5~7", correlation="LL,RR", mode=mode, action="apply", flagbackup=False)
+    flagdata(vis=selfcal_vis, datacolumn="data", spw="5~7", mode="extend", extendflags=False, action="apply", flagbackup=False)
+
+    # Backup MS to the state after self-cal
+    flagmanager(vis=visfile, mode='save', versionname='after_selfcal_flagging')
+
+    # Run phase-loop self-calibration
     phscal.run()
 
     # ampcal = Ampcal(minsnr=2.0, solint=solint_amp, combine="scan",
