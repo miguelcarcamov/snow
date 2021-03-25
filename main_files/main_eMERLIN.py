3eb9353c86a12e52b70f3c301749636fe3dcba71
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2021-03-25 13:35:37 -0300

    ADD: changing eMERLIN script

--- main_files/main_eMERLIN.py
+++ main_files/main_eMERLIN.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     if not os.path.exists(selfcal_vis): mstransform(vis=visfile, outputvis=selfcal_vis, datacolumn="corrected", field=field)
 
     clean_imager = Clean(inputvis=selfcal_vis, output=output, niter=10000, M=1024, N=1024, cell="0.02arcsec",
-                         stokes="I", datacolumn="corrected", robust=2.0, scales=[0, 3, 5, 10 ,15, 20, 30, 40, 50, 80], specmode="mfs", deconvolver="multiscale", gridder="standard",
-                         pbcor=False, savemodel=True, nsigma=3.0, interactive=False, cycleniter=100, usemask=output+"_ph2.mask")
+                         stokes="I", datacolumn="corrected", robust=2.0, scales=[0, 3, 5, 10 ,15, 20, 30, 40, 50, 80, 100], specmode="mfs", deconvolver="multiscale", gridder="standard",
+                         pbcor=False, savemodel=True, nsigma=3.0, interactive=False, cycleniter=100, usemask='user', mask=output+"_ph2.mask")
 
     shared_vars_dict = {'visfile': clean_imager.getVis(),
                         'minblperant': 2,
@@ -37,14 +37,16 @@ if __name__ == '__main__':
     #solint_phs = ['128s', '64s', '32s', '16s']
     solint_phs = ['3.5min', '2min', '1min', '30s', '15s']
     #varchange_phs = {'nsigma' : [4.0, 3.0, 3.0, 2.0, 1.0]}
-    varchange_phs = {'nsigma' : [2.0]}
     #solint_amp = ['1h']
+    varchange_ap = {'nsigma' : [2.0, 2.0, 2.0],
+                    'usemask' : ['user', 'user', 'auto-multithresh'],
+                    'mask' : [output+'_ph2.mask', output+'_ph2.mask, '']}
     solint_ap = ['14min', '7min', '3.5min']
 
     #phscal = Phasecal(minsnr=2.0, solint=solint_phs,
     #                  combine="spw", varchange=varchange_phs, Imager=clean_imager, **shared_vars_dict)
 
-    apcal = AmpPhasecal(minsnr=3.0, solint=solint_ap, combine="spw", input_caltable="pcal1", Imager=clean_imager, uvrange=">60klambda", **shared_vars_dict)
+    apcal = AmpPhasecal(minsnr=3.0, solint=solint_ap, varchange=varchange_ap, combine="spw", input_caltable="pcal1", applymode="calonly", Imager=clean_imager, uvrange=">60klambda", **shared_vars_dict)
 
     if flagging:
         # Backup MS to the state before self-cal
