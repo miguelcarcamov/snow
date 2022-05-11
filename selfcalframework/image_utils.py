adding new psnr formula corrected by the square root of the number of antennas

selfcalframework/image_utils.py
@@ -1,8 +1,10 @@
 from astropy.io import fits
-from casatasks import split
 from casatasks import exportfits
 import numpy as np
+from casatools import table
+import os
 
+tb = table()
 
 def get_data(fits_name=""):
     image_data = fits.getdata(fits_name)
@@ -16,16 +18,29 @@ def exportMStoFITS(msname=""):
     return fitsfile_name
 
 
-def calculatePSNR_FITS(signal_fits_name="", residual_fits_name="", pixels=80):
+def calculatePSNR_FITS(signal_fits_name="", residual_fits_name="", pixels=100):
     signal_data = get_data(signal_fits_name)
     res_data = get_data(residual_fits_name)
-    peak = np.max(np.max(signal_data))
+    peak = np.max(signal_data)
     stdv = np.std(res_data[0:pixels, 0:pixels])
     return peak / stdv, peak, stdv
 
 
-def calculatePSNR_MS(signal_ms_name="", residual_ms_name="", pixels=80):
+def calculatePSNR_MS(signal_ms_name="", residual_ms_name="", pixels=100):
     fits_signal = exportMStoFITS(msname=signal_ms_name)
     fits_residual = exportMStoFITS(msname=residual_ms_name)
     psnr, peak, stdv = calculatePSNR_FITS(fits_signal, fits_residual, pixels)
     return psnr, peak, stdv
+
+def calculate_number_antennas(ms_name=""):
+    if ms_name != "":
+        if os.path.exists(ms_name):
+            tb.open(tablename=ms_name+"/ANTENNA")
+            query_table = tb.taql("select NAME from "+ms_name+"/ANTENNA"+" where !FLAG_ROW")
+            nrows = len(query_table.getcol("NAME"))
+            tb.close()
+            return nrows
+        else:
+            raise FileNotFoundError("The Measurement Set File does not exist")
+    else:
+        raise ValueError("Measurement Set File cannot be empty")

selfcalframework/imager.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
 import sys
-import time
 from casatasks import tclean
 from casatasks import fixvis
 from casatasks import importfits
@@ -31,6 +30,7 @@ class Imager(metaclass=ABCMeta):
 
         if self.noise_pixels is None:
             self.noise_pixels = int(np.floor(M/5))
+        self.nantennas = calculate_number_antennas(inputvis)
         # self.__dict__.update(kwargs)
 
     def getVis(self):
@@ -85,7 +85,7 @@ class Imager(metaclass=ABCMeta):
     def getVerbose(self):
         return self.verbose
 
-    def setVerbose(self):
+    def setVerbose(self, verbose=True):
         self.verbose = verbose
 
     def getOutputPath(self):
@@ -108,19 +108,25 @@ class Imager(metaclass=ABCMeta):
 
     def calculateStatistics_FITS(self, signal_fits_name="", residual_fits_name="", stdv_pixels=None):
         if stdv_pixels is None:
-            self.psnr, self.peak, self.stdv = calculatePSNR_FITS(
+            psnr, peak, stdv = calculatePSNR_FITS(
                 signal_fits_name, residual_fits_name, self.noise_pixels)
         else:
-            self.psnr, self.peak, self.stdv = calculatePSNR_FITS(
+            psnr, peak, stdv = calculatePSNR_FITS(
                 signal_fits_name, residual_fits_name, stdv_pixels)
+
+            stdv_corrected = stdv * np.sqrt(self.nantennas-3)
+            self.psnr = peak / stdv_corrected
+            self.peak = peak
+            self.stdv = stdv_corrected
 
     def calculateStatistics_MSImage(self, signal_ms_name="", residual_ms_name="", stdv_pixels=None):
         if stdv_pixels is None:
-            self.psnr, self.peak, self.stdv = calculatePSNR_MS(
+            psnr, peak, stdv = calculatePSNR_MS(
                 signal_ms_name, residual_ms_name, self.noise_pixels)
-        else:
-            self.psnr, self.peak, self.stdv = calculatePSNR_FITS(
-                signal_fits_name, residual_fits_name, stdv_pixels)
+            stdv_corrected = stdv * np.sqrt(self.nantennas - 3)
+            self.psnr = peak / stdv_corrected
+            self.peak = peak
+            self.stdv = stdv_corrected
 
     @abstractmethod
     def run(self, imagename=""):