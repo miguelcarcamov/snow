c10e4fa32f9594029d726c0dcc1b4203fc78cd19
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-06-22 16:17:15 +0100

    FIX:refactoring imaging

--- selfcalframework/imaging/wsclean.py
+++ selfcalframework/imaging/wsclean.py
@@ -4,12 +4,7 @@ from .imager import Imager
 class WSClean(Imager):
 
     def __init__(self, **kwargs):
-        super(WSClean, self).__init__(**kwargs)
-        initlocals = locals()
-        initlocals.pop('self')
-        for a_attribute in initlocals.keys():
-            setattr(self, a_attribute, initlocals[a_attribute])
-        self.__dict__.update(kwargs)
+        super().__init__(**kwargs)
 
     def run(self, imagename=""):
-        return
+        pass
