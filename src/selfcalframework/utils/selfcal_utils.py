d4afafe6992a72f06292f9582744ccb9241a2d77
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-07-16 10:35:35 +0100

    CHG: adding comments to selfcal class

--- src/selfcalframework/utils/selfcal_utils.py
+++ src/selfcalframework/utils/selfcal_utils.py
@@ -5,14 +5,37 @@ from casatools import table
 tb = table()
 
 
-def getTableRows(mstable=""):
-    tb.open(tablename=mstable)
+def get_table_rows(ms_table: str = "") -> int:
+    """
+    Function that returns the number of rows of a measurement set table
+
+    Parameters
+    ----------
+    ms_table : Measurement set table name
+
+    Returns
+    -------
+    The number of rows of the measurement set table
+
+    """
+    tb.open(tablename=ms_table)
     rows = tb.nrows()
     tb.close()
     return rows
 
 
-def calculate_number_antennas(ms_name: str = ""):
+def calculate_number_antennas(ms_name: str = "") -> int:
+    """
+    Function that calculates the number of non-flagged antennas of a measurement set file
+
+    Parameters
+    ----------
+    ms_name : Absolute file name to the measurement set file
+
+    Returns
+    -------
+    Number of non-flagged antennas
+    """
     if ms_name != "":
         if os.path.exists(ms_name):
             tb.open(tablename=ms_name + "/ANTENNA")
