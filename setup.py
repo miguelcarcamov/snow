1f8a5ee53377ceca802a461b4db59991a19a5678
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-10-30 18:00:22 +0000

    CHG: adding pyralysis as restoring algorithm when using gpuvmem

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import distutils.text_file
 from pathlib import Path
 from typing import List
 
@@ -7,13 +6,56 @@ from setuptools import setup
 this_directory = Path(__file__).parent
 
 
-def _parse_requirements(filename: str) -> List[str]:
-    """Return requirements from requirements file."""
-    # Ref: https://stackoverflow.com/a/42033122/
-    return distutils.text_file.TextFile(filename=str(Path(__file__).with_name(filename))
-                                        ).readlines()
+def get_requirements(filename: str, remove_links=True) -> List[str]:
+    """
+    lists the requirements to install.
+    """
+    with open(filename) as f:
+        requirements = f.read().splitlines()
+    if remove_links:
+        for requirement in requirements:
+            # git repository url.
+            if requirement.startswith("git+"):
+                requirements.remove(requirement)
+            # subversion repository url.
+            if requirement.startswith("svn+"):
+                requirements.remove(requirement)
+            # mercurial repository url.
+            if requirement.startswith("hg+"):
+                requirements.remove(requirement)
+    return requirements
+
+
+def get_links(filename: str) -> List[str]:
+    """
+    gets URL Dependency links.
+    """
+    links_list = get_requirements(filename, remove_links=False)
+    for link in links_list:
+        keep_link = False
+        already_removed = False
+        # git repository url.
+        if not link.startswith("git+"):
+            if not link.startswith("svn+"):
+                if not link.startswith("hg+"):
+                    links_list.remove(link)
+                    already_removed = True
+                else:
+                    keep_link = True
+                if not keep_link and not already_removed:
+                    links_list.remove(link)
+                    already_removed = True
+            else:
+                keep_link = True
+            if not keep_link and not already_removed:
+                links_list.remove(link)
+    return links_list
 
 
 if __name__ == "__main__":
     use_scm_version = {"root": ".", "relative_to": __file__, "local_scheme": "no-local-version"}
-    setup(use_scm_version=use_scm_version, install_requires=_parse_requirements("requirements.txt"))
+    setup(
+        use_scm_version=use_scm_version,
+        install_requires=get_requirements("requirements.txt"),
+        dependency_links=get_links("requirements.txt")
+    )
